8ba22605649b8ba89d947ed9fca28e78b28aea64
Michael Rose <Michael.Rose@theice.com>
2018-05-02 20:22:52 -0400

    Increment package version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='robotframework-snowlibrary',
-    version='0.1.0',
+    version='0.2.2',
     url='',
     license='Apache License 2.0',
     author='Michael Rose',
