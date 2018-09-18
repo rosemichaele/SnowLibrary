7d94fa0336650c9aa7d0731d7a3822e8aae20466
Yuting <Yuting.wang@theice.com>
2018-09-18 15:40:50 -0400

    Modify the existing keywords and add a new one in notification_helper.py

--- src/SnowLibrary/keywords/notification_helper.py
+++ src/SnowLibrary/keywords/notification_helper.py
@@ -13,25 +13,24 @@ class NotificationHelper:
 
     ROBOT_LIBRARY_SCOPE = "TEST CASE"
 
-    @keyword
-    def get_expected_notification_message(self, number, state, business, application, severity, description, type, work_note=None):
+    def _oir_number_transform(self,number):
         """
-        Used to get the beginning part of expected message format.
-        :param number: The transformed OIR number.
-        :param state: The transformed state.
-        :param business: The business OIR is related to.
-        :param application: The application OIR is related to.
-        :param severity: The transformed severity.
-        :param description: The oir description.
-        :param type: The message type: email or sms.
-        :return: The expected message format. 
+        Used to transfer number to expected format.
+        :param number: The OIR record number.
+        :return: The number only with digits. 
         """
-        # Transform the number to expected format
         if (len(number) != 10):
             raise AssertionError("Please enter the correct number, which is expected to have 10 digits.")
         else:
-            self.sms_number = number[3:]
-        #Transform the severity to expected format
+            self.expected_number = number[3:]
+        return self.expected_number
+
+    def _oir_severity_transform(self,severity):
+        """
+        Used to transfer severity to expected format.
+        :param severity: The OIR record severity.
+        :return: The expected format to present severity in notification message.
+        """
         if (severity == "5"):
             self.sev = "SEV5"
         elif (severity == "4"):
@@ -44,14 +43,53 @@ class NotificationHelper:
             self.sev = "SEV1"
         else:
             raise AssertionError("Please enter the correct severity, which is the number from 1~5.")
-        #Transform the state to expected format
+        return self.sev
+
+    def _oir_state_transform(self,state):
+        """
+        Used to transfer state to expected format.
+        :param state: The OIR record state.
+        :return: The expected format to present state in notification message.
+        """
         if (state == "Work in Progress"):
             self.stat = "WIP"
         elif (state == "Root Cause Pending"):
             self.stat = "RCP"
         else:
             raise AssertionError("Please enter the correct state, which is Work in Progress or Root Cause Pending.")
-        #Get the expected message format
+        return self.stat
+
+    def _active_bridge_number_transform(self,domestic,access_code,pin_code):
+        """
+        Used to transfer active bridge number to expected format.
+        :param domestic: Domestic number of reserved bridge.
+        :param access_code: Access code of reserved bridge.
+        :param pin_code: Pin code of reserved bridge.
+        :return: The expected format to present bridge info in notification message.
+        """
+        if((domestic==None) or (access_code==None) or (pin_code==None)):
+            raise AssertionError("Please enter domestic number, access code and pin code")
+        else:
+            self.bridge_number=domestic + ",,"+access_code+"#,,"+pin_code+"#"
+        return self.bridge_number
+
+    @keyword
+    def get_expected_notification_message(self, number, state, business, application, severity, description, type, work_note=None):
+        """
+        Used to get the expected automatic notification message format.
+        :param number: The transformed OIR number.
+        :param state: The transformed state.
+        :param business: The business OIR is related to.
+        :param application: The application OIR is related to.
+        :param severity: The transformed severity.
+        :param description: The oir description.
+        :param type: The message type: email or sms.
+        :return: The expected message format. 
+        """
+        self.sms_number=self._oir_number_transform(number)
+        self.stat=self._oir_state_transform(state)
+        self.sev=self._oir_severity_transform(severity)
+
         if(work_note==None):
             if(type=="sms"):
                 return self.sms_number + "|" + self.sev + "|" + business + "|" + self.stat + "|" + description
@@ -74,6 +112,34 @@ class NotificationHelper:
         :param info: The info get from Notification Preferences page.
         :return: User's telephone number.
         """
-        number = info.split(" ")
-        return number[1]
+        self.number = info.split(" ")
+        return self.number[1]
+
+    @keyword
+    def get_send_notification_expected_message(self,number,state,severity,business,application,description,domestic,access_code,pin_code,type):
+        """
+        Used to get the expected send notification message format.
+        :param number: OIR record number
+        :param state: OIR record state
+        :param severity: OIR record severity
+        :param business: OIR record business
+        :param description: OIR record description
+        :param application: OIR record application.
+        :param bridge_number: OIR record reserved bridge number.
+        :param type: notification message type: sms or email.
+        :return: The expected message format.
+        """
+        self.sms_number = self._oir_number_transform(number)
+        self.stat = self._oir_state_transform(state)
+        self.sev = self._oir_severity_transform(severity)
+        self.bridge_number = self._active_bridge_number_transform(domestic,access_code,pin_code)
+
+        if (type == "sms"):
+            return self.sms_number + "|" + self.sev + "|" + business + "|" + self.stat + "||" + self.bridge_number
+        elif (type == "email"):
+            return number + " " + self.sev + " " + business + " " + self.stat + " " + application + " | " + description
+        else:
+            raise AssertionError("Please enter the correct type, which is sms or email.")
+
+
 
